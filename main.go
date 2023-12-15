96e096a996dfcca8d2b4b808be47023aee31e69a
MoAtefAmer <mo.atefamer@gmail.com>
2023-12-15 14:48:01 +0200

    Refactor health check and server management

--- main.go
+++ main.go
@@ -5,37 +5,78 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"time"
 )
 
 var (
-	listOfServers = []string{"http://localhost:5201", "http://localhost:5200","http://localhost:5555"}
-	currentIndex  = 0
-	mutex         = &sync.Mutex{}
+	allServers     = []string{"http://localhost:5201", "http://localhost:5200", "http://localhost:5555"} // Full list of servers
+	listOfServers  = []string{} // List of servers that are up
+	downedServers  = []string{} // List of servers that are down
+	mutex          = &sync.Mutex{}
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Received request from %s\n", r.RemoteAddr)
+func healthCheck() {
+	fmt.Println("Starting health check...")
 
+	for range time.Tick(10 * time.Second) { // Run health check every 10 seconds
 		mutex.Lock()
-		server := listOfServers[currentIndex]
-		fmt.Println("Current Index", currentIndex)
-		currentIndex = (currentIndex + 1) % len(listOfServers)
-		mutex.Unlock()
+		var tempDownedServers []string
 
-		fmt.Printf("Redirecting request to %s\n", server)
-		http.Redirect(w, r, server, 301)
+		// Use http.Client with a timeout
+		client := http.Client{
+			Timeout: 5 * time.Second,
+		}
 
-		fmt.Printf("%s %s %s\n", r.Method, r.URL.Path, r.Proto)
-		for name, headers := range r.Header {
-			for _, h := range headers {
-				fmt.Printf("%v: %v\n", name, h)
+		// Check all servers every time
+		for _, server := range allServers {
+			_, err := client.Get(server)
+			if err != nil {
+				fmt.Println("-----------------------------------")
+				fmt.Printf("Server %s is down\n", server)
+				tempDownedServers = append(tempDownedServers, server)
+			} else {
+				fmt.Println("-----------------------------------")
+				fmt.Printf("Server %s is up\n", server)
 			}
 		}
 
-		// Respond with a hello message
+		// Update the lists with the current state of the servers
+		listOfServers = []string{}
+		downedServers = []string{}
+
+		// Decide which servers are up and which are down
+		for _, server := range allServers {
+			foundDown := false
+			for _, downedServer := range tempDownedServers {
+				if server == downedServer {
+					foundDown = true
+					break
+				}
+			}
+			if foundDown {
+				downedServers = append(downedServers, server)
+			} else {
+				listOfServers = append(listOfServers, server)
+			}
+		}
+
+		mutex.Unlock()
+	}
+}
+
+func main() {
+	// Initialize listOfServers with allServers
+	listOfServers = allServers
+
+	// Start the health check in the background
+	go healthCheck()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		mutex.Lock() // Lock the mutex before accessing shared variables
 		fmt.Fprintln(w, "Hello From Backend Server")
-		fmt.Println("Replied with a hello message")
+		fmt.Fprintln(w, "Available servers:", listOfServers)
+		fmt.Fprintln(w, "Downed servers:", downedServers)
+		mutex.Unlock() // Unlock the mutex when done accessing shared variables
 	})
 
 	fmt.Println("Backend server is starting...")
@@ -43,4 +84,6 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+	// The "Backend server is shutting down..." log is unnecessary here,
+	// as ListenAndServe will block indefinitely until an error occurs.
 }
